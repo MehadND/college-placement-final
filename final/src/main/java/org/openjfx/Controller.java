cebe2215331e5a5324ae367131db92e814174a31
MehadND <mehadnd@gmail.com>
2022-04-20 18:34:28 +0100

    New Project Created (with JavaFX & Maven)

---
+++ final/src/main/java/org/openjfx/Controller.java
@@ -0,0 +1,77 @@
+package org.openjfx;
+
+import java.awt.Desktop;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.Properties;
+
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Controller {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	@FXML
+	private Button goToLoginButton;
+
+	@FXML
+	private Button goToAppButton;
+	
+	public void switchToScene1(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("BeyondTravel Login");
+		stage.show();
+	}
+
+	public void switchToScene2(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/Application.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("BeyondTravel Application");
+		stage.show();
+
+	}
+}
