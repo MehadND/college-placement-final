77fd53f2418ec0908fc966f587532bc5f9b5ebc5
MehadND <mehadnd@gmail.com>
2022-04-24 20:33:08 +0100

    Added JFoenix maven dependency for better GUI components + Added icon for application
    - JFoenix dependency allows us to create nice looking gui components e.g. the email and password fields.
    - Our application now has icon on top left of window as well as an icon on taskbar.
    - made few minor changes to gui styling

--- final/src/main/java/org/openjfx/Main.java
+++ final/src/main/java/org/openjfx/Main.java
@@ -6,6 +6,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -28,7 +29,7 @@ public class Main extends Application {
 					primaryStage.setY(dragEvent.getScreenY() - pressEvent.getSceneY());
 				});
 			});
-
+			primaryStage.getIcons().add(new Image("ApplicationIcon.png"));
 			
 			//WindowStyle.allowDrag(root, primaryStage);
 			primaryStage.show();
